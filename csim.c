473e1798a7ca8ae99294c46725ad1267a9ac1fef
bill-ginger <jht2001120@163.com>
2021-08-22 21:05:54 +0800

    finish part A

--- csim.c
+++ csim.c
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include "cachelab.h"
 
+
 typedef uint64_t mem_addr_t;
 typedef struct {
     char valid;
@@ -14,25 +15,33 @@ typedef struct {
 typedef cache_line_t* cache_set_t;
 typedef cache_set_t*  cache_t; 
 
-uint32_t lru = -1;
+
 uint8_t verbose = 0;
-int s, b, E, S, B;
+uint64_t s, b, E, S, B;
 char* trace_file;
 cache_t cache;
+uint64_t hit_cnt, miss_cnt, evict_cnt, lru_cnt;
+
 
 void print_help(char * const *);
 void parse_args(int, char * const *);
 void init_cache();
+void free_cache();
+void replay(char *);
+void add_data(mem_addr_t);
 
-int main(int argc, char* const argv[])
-{
+
+int main(int argc, char* const argv[]) {
     parse_args(argc, argv);
     init_cache();
-    printSummary(0, 0, 0);
+    replay(trace_file);
+    free_cache();
+    printSummary(hit_cnt, miss_cnt, evict_cnt);
     return 0;
 }
 
-void print_help(char * const argv[]){
+
+void print_help(char * const argv[]) {
     printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
     printf("Options:\n");
     printf("  -h         Print this help message.\n");
@@ -46,10 +55,11 @@ void print_help(char * const argv[]){
     printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
 }
 
-void parse_args(int argc, char * const argv[]){
+
+void parse_args(int argc, char * const argv[]) {
     char opt;
-    while((opt = getopt(argc, argv, "s:b:E:t:hv")) != EOF){
-        switch (opt){
+    while((opt = getopt(argc, argv, "s:b:E:t:hv")) != EOF) {
+        switch (opt) {
             case 'h':
                 print_help(argv);
                 exit(0);
@@ -58,7 +68,6 @@ void parse_args(int argc, char * const argv[]){
                 break;
             case 's':
                 s = atoi(optarg);
-                printf("%d\n", s);
                 break;
             case 'b':
                 b = atoi(optarg);
@@ -68,24 +77,101 @@ void parse_args(int argc, char * const argv[]){
                 break;
             case 't':
                 trace_file = optarg;
+                break;
         }
     }
-    if (!s || !b || !E || !trace_file){
+    if (!s || !b || !E || !trace_file) {
         fprintf(stderr, "%s: Please enter command line arguments!\n", argv[0]);
         print_help(argv);
         exit(1);
     }
 }
 
-void init_cache(){
-    S = 2 << s; B = 2 << b;
-    cache = (cache_t)malloc(S * sizeof(cache_set_t));
-    for (int i = 0; i < S; ++i){
-        cache[i] = (cache_set_t)malloc(E * sizeof(cache_line_t));
-        for (int j = 0; j < E; ++j){
+
+void init_cache() {
+    S = 1 << s; B = 1 << b;
+    cache = (cache_t) malloc(S * sizeof(cache_set_t));
+    for (int i = 0; i < S; ++i) {
+        cache[i] = (cache_set_t) malloc(E * sizeof(cache_line_t));
+        for (uint64_t j = 0; j < E; ++j) {
             cache[i][j].valid = 0;
             cache[i][j].tag = 0;
             cache[i][j].lru = 0;
         }
     }
+}
+
+
+void free_cache() {
+    for (int i = 0; i < S; ++i) {
+        free(cache[i]);
+    }
+    free(cache);
+}
+
+
+void replay(char* trace_file) {
+    FILE * fp = NULL;
+    fp = fopen(trace_file, "r");
+    if (fp == NULL) {
+        perror(trace_file);
+        exit(1);
+    } 
+    char buf[100];
+    while(fgets(buf, 90, fp) != NULL) {
+        if (buf[0] == 'I') {
+            continue;
+        }
+        mem_addr_t addr; uint32_t size;
+        sscanf(buf+3, "%lx,%u", &addr, &size);
+        if (verbose) {
+            printf("%c %lx,%u ", buf[1], addr, size);
+        }
+        switch (buf[1]) {
+            case 'M':
+                add_data(addr);
+            case 'L':
+            case 'S':
+                add_data(addr);
+        }
+        if (verbose) {
+            printf("\n");
+        }
+    }
+}
+
+
+void add_data(mem_addr_t addr) {
+    cache_set_t cache_set = cache[ (addr >> b) & (S - 1) ];
+    uint64_t tag = addr >> (s + b);
+    for (int i = 0; i < E; ++i) {
+        if (tag == cache_set[i].tag && cache_set[i].valid) {
+            ++hit_cnt;
+            if (verbose){
+                printf("hit ");
+            }
+            cache_set[i].lru = lru_cnt++;
+            return;
+        }
+    }
+    ++miss_cnt;
+    if (verbose) {
+        printf("miss ");
+    }
+    uint32_t min_lru = -1, line; 
+    for (int i = 0; i < E; ++i) {
+        if (cache_set[i].lru < min_lru) {
+            line = i;
+            min_lru = cache_set[i].lru;
+        }
+    }
+    if (cache_set[line].valid) {
+        ++evict_cnt;
+        if (verbose) {
+            printf("eviction ");
+        }
+    }
+    cache_set[line].valid = 1;
+    cache_set[line].tag = tag;
+    cache_set[line].lru = lru_cnt++;
 }
